fc8f6ecc79a2f68f23982369f3478dfdc7ed8886
My1nonly <hafizdiangga@gmail.com>
2022-07-02 07:57:46 +0700

    practicum3_Reporting

--- academicIS/resources/articles/index.blade.php
+++ academicIS/resources/articles/index.blade.php
@@ -5,12 +5,20 @@
             <p>{{ $message }}</p>
         </div>
     @endif
+
+    <div class="row">
+        <div style="margin:0px 0px 0px 70px;">
+            <a class="btn btn-success" href="{{route('print_pdf)}}">Print PDF</a>
+    </div>
+</div><br/>
+
     <table class="table table-bordered" style="width:95%;margin: 0 auto;">
     <tr>
         <th>Title</th>
         <th>Content</th>
         <th>Image</th>
     </tr>
+    
 @foreach ($articles as $article)
     <tr>
         <td><a href="{{route('articles.edit', ['article'=>$article->id]) }}">{{ $article->title }}</a></td>
--- academicIS/routes/web.php
+++ academicIS/routes/web.php
@@ -21,3 +21,5 @@ Route::resource('student', StudentController::class);
 
 use App\Http\Controllers\ArticleController;
 Route::resource('articles', ArticleController::class);
+
+route::get('/article/print_pdf', [ArticleController::class, 'print_pdf'])->name('print_pdf');
